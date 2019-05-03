b0ce1eefe0af7ed04454266356aa56636bc60f9e
Xinke Chen <xinkecf35@gmail.com>
2019-05-03 01:12:18 -0400

    Initial Commit

---
+++ ARPittITHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPittITHelper
+//
+//  Created by Xinke Chen on 2019-05-03.
+//  Copyright © 2019 Xinke Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
